96252af8aa38d7cbf7ca9deba8f7eb50f3ac35d4
mchhour31 <marlon1293@gmail.com>
2022-12-15 15:39:41 +1300

    refactored p2 day2 aoc

--- day2/day2.py
+++ day2/day2.py
@@ -22,82 +22,14 @@ for o, r in t:
 print(s)
 
 # part 2
+new_outcome = {
+    'X': {'status': 0, 'A': 3, 'B': 1, 'C': 2},
+    'Y': {'status': 3, 'A': 1, 'B': 2, 'C': 3},
+    'Z': {'status': 6, 'A': 2, 'B': 3, 'C': 1}
+}
 
+s = 0
+for o, r in t:
+    s += new_outcome[r]['status'] + new_outcome[r][o]
 
-# total_1 = 0
-# total_2 = 0
-# selected_1 = 0
-# selected_2 = 0
-# outcome_1 = 0
-# outcome_2 = 0
-
-# # # part1 - naive approach
-# for a, x in t:
-#     if a == 'A' and x == 'Z':
-#         selected_1, selected_2 = 1, 3
-#         outcome_1, outcome_2 = 6, 0
-#     elif a == 'A' and x == 'Y':
-#         selected_1, selected_2 = 1, 2
-#         outcome_1, outcome_2 = 0, 6
-#     elif a == 'A' and x == 'X':
-#         selected_1, selected_2 = 1, 1
-#         outcome_1, outcome_2 = 3, 3
-    
-#     if a == "B" and x == 'Z':
-#         selected_1, selected_2 = 2, 3
-#         outcome_1, outcome_2 = 0, 6
-#     elif a == "B" and x == 'Y':
-#         selected_1, selected_2 = 2, 2
-#         outcome_1, outcome_2 = 3, 3
-#     elif a == "B" and x == 'X':
-#         selected_1, selected_2 = 2, 1
-#         outcome_1, outcome_2 = 6, 0
-        
-#     if a == 'C' and x == 'Z':
-#         selected_1, selected_2 = 3, 3
-#         outcome_1, outcome_2 = 3, 3
-#     elif a == 'C' and x == 'Y':
-#         selected_1, selected_2 = 3, 2
-#         outcome_1, outcome_2 = 6, 0
-#     elif a == 'C' and x == 'X':
-#         selected_1, selected_2 = 3, 1
-#         outcome_1, outcome_2 = 0, 6
-    
-#     total_1 += selected_1 + outcome_1
-#     total_2 += selected_2 + outcome_2
-
-# print(total_1, total_2)
-
-# # part2
-# d = {'X': 0, 'Y': 3, 'Z': 6}
-
-# s = 0
-
-# for a, x in t:
-#     if a == 'A':
-#         if d[x] == 0:
-#             z = 3
-#         elif d[x] == 3:
-#             z = 1
-#         elif d[x] == 6:
-#             z = 2
-            
-#     elif a == 'B':
-#         if d[x] == 0:
-#             z = 1
-#         elif d[x] == 3:
-#             z = 2
-#         elif d[x] == 6:
-#             z = 3
-    
-#     elif a == 'C':
-#         if d[x] == 0:
-#             z = 2
-#         elif d[x] == 3:
-#             z = 3
-#         elif d[x] == 6:
-#             z = 1
-    
-#     s += d[x] + z
-
-# print(s)
+print(s)
